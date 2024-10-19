c412fbb7e3e22ce340d58e9c7980c0d3f679a357
Nick Marino <nick.marino126@gmail.com>
2024-10-19 16:49:08 -0400

    Idle reconnect functionality

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import os
 
 from pathlib import Path
 from providers.imap_provider import ImapProvider
+from concurrent.futures import ThreadPoolExecutor
+
 
 ACCOUNTS_DIR = 'config/accounts'
 
@@ -9,4 +11,5 @@ accounts = Path(ACCOUNTS_DIR)
 
 for account in accounts.iterdir():
     imap_provider = ImapProvider.create_imap_providers(account)
-    imap_provider.listen()
+    ThreadPoolExecutor(max_workers=1).submit(imap_provider.listen)
+
--- providers/imap_provider.py
+++ providers/imap_provider.py
@@ -4,8 +4,7 @@ import time
 import os
 import yaml
 from abc import ABC, abstractmethod
-from concurrent.futures import ThreadPoolExecutor
-from enum import Enum
+from datetime import datetime
 from filters.filteractions import FilterAction, FilterGroup
 from imapclient import IMAPClient, exceptions
 from typing import override
@@ -18,20 +17,22 @@ logger = logging.getLogger(__name__)  # Get the logger for this module
 
 class ImapProvider(ABC):
     EMAIL_DATA_FORMAT = "RFC822"
+    EMAIL_EXISTS = "EXISTS"
+    MAX_CONNECT_TIME = 1500
 
     def __init__(self, auth, filter_group, imap_server_url):
-        logger.info(f"Initializing IMAP provider for folder: {self._filter_group.watching_folder}")
+        logger.info(f"Initializing IMAP provider for folder: {filter_group.watching_folder}")
 
         self._server = IMAPClient(imap_server_url, use_uid=True)
         self._filter_group = filter_group
 
-        self._connect(auth)
+        self.connect(auth)
         self._watch_folder()
 
     @abstractmethod
-    def _connect(self, auth):
+    def connect(self, auth):   
         raise NotImplementedError
-    
+        
     def get_email_data(self, emails):
         logger.info(f"Fetching data for {len(emails)} emails.")
         email_data = {}
@@ -60,12 +61,8 @@ class ImapProvider(ABC):
         raise NotImplementedError
 
     @abstractmethod
-    def _listen(self):
-        raise NotImplementedError
-
     def listen(self):
-        """Wrapper to start _listen in the background."""
-        ThreadPoolExecutor(max_workers=1).submit(self._listen)
+        raise NotImplementedError
             
     @staticmethod
     def create_imap_providers(account):
@@ -89,8 +86,10 @@ class GmailIMAPProvider(ImapProvider):
         logger.info(f"Gmail IMAP Provider initialized for folder: {filter_group.watching_folder}")
         super().__init__(auth, filter_group, self.GMAIL_IMAP_SERVER_URL)
 
+        self._connect_time = 0
+
     @override
-    def _connect(self, auth):
+    def connect(self, auth):
         logger.info("Connecting to Gmail IMAP server.")
         try:
             with open(auth, 'r') as file:
@@ -121,37 +120,47 @@ class GmailIMAPProvider(ImapProvider):
             self._server.delete_messages(emails)
             self._server.uid_expunge(emails)
             logger.info("Emails deleted: {}".format(emails))
-        self._server.select_folder(self._filter_group.watching_folder, readonly=True)    # Close folder with "readOnly" when finished
+        self._server.select_folder(self._filter_group.watching_folder, readonly=True)    # Close folder with "readonly" when finished
+
+    @override
+    def listen(self):
+        logger.info(f"Started listening for new emails in folder: {self._filter_group.watching_folder}")
+        
+        while True:
+            try:
+                self._idle_connect()
+                idle_response = self._server.idle_check(timeout=5)
+
+                self._handle_response(idle_response)
+            except Exception as e:
+                logger.error(f"Error during listening: {e}")
+                raise
+            
+    def _idle_connect(self):
+        if self._connect_time == 0:
+            self._server.idle()
+        else:
+            time_difference = datetime.now() - self._connect_time
+
+            if time_difference >= self.MAX_CONNECT_TIME:
+                self._server.idle_done()
+                self._server.idle()
+                self._connect_time = datetime.now()
+
+    def _idle_disconnect(self):
+        self._server.idle_done()
+        self._connect_time = 0
 
     def _handle_response(self, idle_response):
         if idle_response:
             new_emails = list(map(lambda email: email[0], \
-                list(filter(lambda response: b'EXISTS' in response, idle_response))))
+                list(filter(lambda response: self.EMAIL_EXISTS.encode() in response, idle_response))))
             if new_emails:
                 logger.info("New emails received: {}".format(new_emails))
-                self._server.idle_done()
+                self._idle_disconnect()
                 for email_filter in self.filters:
                     logger.info("Applying filters")
                     email_filter.apply(new_emails, self)
-                return True
-        return False
-
-    @override
-    def _listen(self):
-        logger.info(f"Started listening for new emails in folder: {self._filter_group.watching_folder}")
-        
-        self._server.idle()
-        while True:
-            try:
-                idle_response = self._server.idle_check()
-                
-                idle_status = self._handle_response(idle_response)
-                if idle_status:
-                    self._server.idle()
-            except Exception as e:
-                logger.error(f"Error during listening: {e}")
-                raise
-
 
  # @override
     # def _idle_check(self):
